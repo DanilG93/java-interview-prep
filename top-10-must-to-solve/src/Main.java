be9106a0a9e4c960888e0fe31541416393a9d84f
Danilo Gomanjuk <danilogomanjuk93@gmail.com>
2026-03-25 19:44:29 +0100

    feat: implement two sum solution using brute force

--- top-10-must-to-solve/src/Main.java
+++ top-10-must-to-solve/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        Solution solution = new Solution();
+
+        int[] nums = new int[]{2,5,5,11};
+        int target = 10;
+
+
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+
 
     }
 }
--- top-10-must-to-solve/src/Solution.java
+++ top-10-must-to-solve/src/Solution.java
@@ -1,7 +1,27 @@
 public class Solution {
 
+/*
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+You can return the answer in any order.
+
+Time Complexity: O(n2)
+Space Complexity: O(1)
+
+ */
+
     public int[] twoSum(int[] nums, int target) {
 
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int y = i+1; y < nums.length; y++) {
+                if (nums[i] + nums[y] == target) {
+                    return new int[]{i, y};
+                }
+            }
+        }
 
         return new int[0];
     }
